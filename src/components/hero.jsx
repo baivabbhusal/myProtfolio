974151faa339f1a26c0deea1baf5307d590fb965
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-09-10 20:37:52 +0545

    updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from "react-typed";
 import SocialIcon from "./socials";
 import BlueButton from "./button";
 import baivabImg from "../assets/myBarcaRight.png";
@@ -6,7 +7,7 @@ const Hero = () => {
   return (
     <div className="container mx-auto px-4 mt-10 rounded-xl">
       <div className="flex flex-col w-full md:flex-row items-center mx-10 rounded-full">
-              <div className="w-1/2 h-auto">
+        <div className="w-1/2 h-auto">
           <img
             src={baivabImg}
             alt="This is image"
@@ -15,13 +16,25 @@ const Hero = () => {
             className="w-full md:w-2/3 h-auto object-contain rounded-3xl p-4"
           ></img>
         </div>
-        
+
         <div className="w-full text-center md:flex flex-col w-1/2">
           <h3 className="text-xl font-bold">Hi,✋ I'm</h3>
           <h1 className="font-bold text-5xl text-primary">Baivab Bhusal</h1>
-          <h1 className="font-bold text-2xl text-secondary">
-            And I'm Frontend Developer
-          </h1>
+          <div className="flex items-center justify-center h-screen bg-gray-900">
+            <h1 className="text-3xl md:text-5xl font-mono text-white">
+              I am a{" "}
+              <Typed
+                strings={[
+                  "Frontend Developer",
+                  "MERN Stack Developer",
+                  "Designer",
+                ]}
+                typeSpeed={70}
+                backSpeed={50}
+                loop
+              />
+            </h1>
+          </div>
           <p className="text-lg mt-4 leading-relaxed">
             I design and build modern, responsive web applications using React,
             Next.js, and Tailwind CSS. Passionate about frontend development, I
@@ -31,11 +44,9 @@ const Hero = () => {
             <SocialIcon />
           </div>
           <div>
-            <BlueButton props={{ buttonName: "Download CV" }}/>
+            <BlueButton props={{ buttonName: "Download CV" }} />
           </div>
         </div>
-
-
       </div>
     </div>
   );
