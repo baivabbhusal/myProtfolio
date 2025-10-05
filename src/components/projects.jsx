d6e43faacf6e9dbc0cea71a1851047a81cb02f2e
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-10-05 20:22:29 +0545

    update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       <div className="flex flex-col justify-between items-center" id="projects">
       <h1 className='text-5xl text-secondary font-bold text-center mb-8'>Projects</h1>
       <div className='grid md:grid-cols-3'>
-        <div>
+        <div className="border-1xl">
           Glacier clothing Ecommerce site
         </div>
 
